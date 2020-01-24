f6dfda8534559606fa7e22c7d89b99aef996811c
kusuma1409 <kusumasri1409@gmail.com>
2020-01-24 18:56:02 +0530

    Initial Commit

---
+++ github2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github2
+//
+//  Created by Yakkala Kusumakumari on 1/24/20.
+//  Copyright © 2020 Yakkala Kusumakumari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
